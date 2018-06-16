6bcf56803da025a55f41be90cd0ff24f5322d267
tangalai <tangalai@hotmail.com>
2018-06-16 00:35:04 -0700

    show tweets

--- src/TweetItem.js
+++ src/TweetItem.js
@@ -3,20 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Twitter from 'twitter';
 
-class App extends Component {
+class TweetItem extends Component {
   constructor(){
     super();
-    console.log("hello world");
-    this.state = {searchtxt:"",
-        user:{
-          id:0,
-          name:"",
-          imageUrl:""
-        },
-        tweets:[]
-      };
-      this.search = this.search.bind(this);
-      this.showTweet = this.showTweet.bind(this);
   }
 
   search(){
@@ -29,14 +18,11 @@ class App extends Component {
     });
     var params = {screen_name: this.state.searchtxt};
     
-    client.get('statuses/user_timeline', params, ((error, tweets, response)=> {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
         console.log(tweets);
-        this.setState({
-          tweets:tweets
-      });
       }
-    }).bind(this));
+    });
 
     client.get('users/show', params, this.UpdateuserInfo.bind(this));
     
@@ -61,28 +47,12 @@ class App extends Component {
     this.setState({searchtxt: event.target.value});
   }
 
-  showTweet(id){
-    var client = new Twitter({
-      consumer_key: 'WsM6Hw9RjAkOgNq9XeHR7D7AB',
-      consumer_secret: 'nQJ7tX7wZSHq9AcQXL6wfXnIzpq7PR0vliS3HjSjizkvo1rbJh',
-      access_token_key: '120942356-Ti37QfqpoTaZILhRcBgnBNzxpIN3DqiN0xgYydmB',
-      access_token_secret: 'A589dOT1tHy6U6DMRX2XROXhQTp3e8D5rGh5ypv97hKld'
-    });
-
-    client.get('statuses/show', {id:id}, ((error, data, response)=> {
-      if (!error) {
-        console.log(data);
-        
-      }
-    }).bind(this));
-  }
-
   render() {
     
-    const tweetsList = this.state.tweets.map((t) =>
-        <div key={t.id} className="tweetItem">
-         <button onClick={()=>{this.showTweet(t.id_str)}}>show</button> {t.created_at} - like Count : {t.favorite_count} - retweet count {t.retweet_count} - {t.text}
-        </div>
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>
+          {number}
+        </li>
       )
 
     return (
@@ -103,8 +73,6 @@ class App extends Component {
             <tr key="t_id"> <td>ID: </td><td>{this.state.user.id}</td> </tr>
             <tr key="t_img"> <td>profile: </td><td> <img src={this.state.user.imageUrl} alt=""></img> </td> </tr></tbody>
             </table>
-           
-            {tweetsList}
           </div>
       </div>
     );
